0c670cff8420d3d471861a8a2e9ca6d2662a11da
fre436 <botpc0018@gmail.com>
2025-10-29 11:09:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 80 },
+    "color": { "value": ["#ff00ff", "#00ffff", "#ff0000"] },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.5 },
+    "size": { "value": 3 },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.3,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" }
+    },
+    "modes": {
+      "repulse": { "distance": 100 }
+    }
+  },
+  "retina_detect": true
+});
